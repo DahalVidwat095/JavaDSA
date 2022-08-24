f35cfef77a4e09217c6aaae2f63b9ffcca4d7380
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-24 12:57:50 +0545

    Odd Number Pyramid

---
+++ src/patterns/OddNumberPyramid.java
@@ -0,0 +1,22 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class OddNumberPyramid {
+    public static void main(String args[]){
+        Scanner cs = new Scanner(System.in);
+        System.out.print(" Enter number of n: ");
+        int n = cs.nextInt();
+        int number = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=number; j++){
+                System.out.print(i+" ");
+            }
+            number = number + 2;
+            System.out.println();
+        }
+    }
+}

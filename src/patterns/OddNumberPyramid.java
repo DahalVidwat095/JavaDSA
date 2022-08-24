60d83c3b1bf711140403a7514af3253756395f91
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-24 13:00:29 +0545

    Odd Number Pyramid

--- src/patterns/OddNumberPyramid.java
+++ src/patterns/OddNumberPyramid.java
@@ -13,7 +13,7 @@ public class OddNumberPyramid {
                 System.out.print("  ");
             }
             for(int j=1; j<=number; j++){
-                System.out.print(i+" ");
+                System.out.print(number+" ");
             }
             number = number + 2;
             System.out.println();

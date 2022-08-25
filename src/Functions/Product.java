fcac99d9fa3bd77f17ede8c6abb01e2beec6b4e1
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-25 15:07:53 +0545

    Product of Two numbers.

---
+++ src/Functions/Product.java
@@ -0,0 +1,19 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class Product {
+    public static int product(int a, int b){
+         int product = a*b;
+         return product;
+    }
+    public static void main(String args[]){
+        Scanner cs = new Scanner(System.in);
+        System.out.print("First number: ");
+        int a = cs.nextInt();
+        System.out.print("Second number: ");
+        int b = cs.nextInt();
+        int product = product(a,b);
+        System.out.print("Product of the given numbers is " + product +".");
+    }
+}

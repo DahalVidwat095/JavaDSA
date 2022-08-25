8f93be337f1852fb8060d19b3663f5d3f7ff68c4
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-25 21:53:20 +0545

    Xmas Tree

--- src/patterns/Xmas.java
+++ src/patterns/Xmas.java
@@ -2,12 +2,25 @@ package patterns;
 
 public class Xmas {
     public static void main(String args[]) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 13 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if(j==1 || j==i || i==7){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9 - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                if(j==1 || j==i || i==9){
+            for (int j = 1; j <= i+4; j++) {
+                if(j==1 || j==i+4 || i==9){
                     System.out.print("* ");
                 }else{
                     System.out.print("  ");
@@ -17,7 +30,7 @@ public class Xmas {
         }
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 9; j++) {
-                if (j == 4 || j == 6) {
+                if (j == 8 || j == 6) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");

405476f29e3e9f38ec3fb92ec58e3df2ccdc7db5
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-26 20:35:12 +0545

    Factorial

---
+++ src/functions/Factorial.java
@@ -0,0 +1,19 @@
+package functions;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void printFactorial(int n) {
+        int factorial = 1;
+        for (int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println("The factorial of " + n + " is " +  factorial +".");
+    }
+    public static void main(String args[]){
+        Scanner cs = new Scanner(System.in);
+        System.out.print("Enter value of n: ");
+        int n = cs.nextInt();
+        printFactorial(n);
+    }
+}

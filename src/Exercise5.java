add3d08e7843c81b2a53d6c8ba24b7c94e722748
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-27 23:44:29 +0545

    Exercise

--- src/Exercise5.java
+++ src/Exercise5.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 public class Exercise5 {
-    public static void main(String args[]) {
-        int dec_num, quot, i = 1, j;
-        int bin_num[] = new int[100];
+    public static void main(String args[])
+    {
+        int dec_num, rem, quot, i=1, j;
+        int oct_num[] = new int[100];
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Input a Decimal Number : ");
+        System.out.print("Input a Decimal Number: ");
         dec_num = scan.nextInt();
 
         quot = dec_num;
 
-        while (quot != 0) {
-            bin_num[i++] = quot % 2;
-            quot = quot / 2;
+        while(quot != 0)
+        {
+            oct_num[i++] = quot%8;
+            quot = quot/8;
         }
 
-        System.out.print("Binary number is: ");
-        for (j = i - 1; j > 0; j--) {
-            System.out.print(bin_num[j]);
+        System.out.print("Octal number is: ");
+        for(j=i-1; j>0; j--)
+        {
+            System.out.print(oct_num[j]);
         }
         System.out.print("\n");
     }
-
 }

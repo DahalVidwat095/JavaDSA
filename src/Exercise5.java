59941c66b4f7aeb8eb6beae87dd6ec22832e33d8
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-27 23:46:27 +0545

    Exercise

--- src/Exercise5.java
+++ src/Exercise5.java
@@ -1,46 +1,17 @@
 import java.util.Scanner;
 
 public class Exercise5 {
-    public static int hex_to_decimal(String s)
-    {
-        String digits = "0123456789ABCDEF";
-        s = s.toUpperCase();
-        int val = 0;
-        for (int i = 0; i < s.length(); i++)
-        {
-            char c = s.charAt(i);
-            int d = digits.indexOf(c);
-            val = 16*val + d;
-        }
-        return val;
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the number of sides on the polygon: ");
+        int ns = input.nextInt();
+        System.out.print("Input the length of one of the sides: ");
+        double side = input.nextDouble();
+        System.out.print("The area is: " + polygonArea(ns, side)+"\n");
     }
-    public static void main(String args[])
-    {
-        String hexdec_num;
-        int dec_num, i=1, j;
-        int octal_num[] = new int[100];
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Input a hexadecimal number: ");
-        hexdec_num = in.nextLine();
-
-        // Convert hexadecimal to decimal
-
-        dec_num = hex_to_decimal(hexdec_num);
-
-        //Convert decimal to octal
-
-        while(dec_num != 0)
-        {
-            octal_num[i++] = dec_num%8;
-            dec_num = dec_num/8;
-        }
-
-        System.out.print("Equivalent of octal number is: ");
-        for(j=i-1; j>0; j--)
-        {
-            System.out.print(octal_num[j]);
-        }
-        System.out.print("\n");
+    public static double polygonArea(int ns, double side) {
+        return (ns * (side * side)) / (4.0 * Math.tan((Math.PI / ns)));
     }
 }

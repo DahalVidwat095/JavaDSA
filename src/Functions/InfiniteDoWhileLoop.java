0bb0cd0435be9942e1abd3b5fe8cd0add9a95b99
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-29 23:44:45 +0545

    E

--- src/Functions/InfiniteDoWhileLoop.java
+++ src/Functions/InfiniteDoWhileLoop.java
@@ -1,6 +1,19 @@
 package Functions;
 
-public class InfiniteDoWhileLoop{
+import java.util.Scanner;
 
-    hello
+public class InfiniteDoWhileLoop{
+/Make a function to check if a given number n is even or not.
+    public static boolean isEven(int number){
+        if(number%2==0){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        Scanner cs = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        int number = cs.nextInt();
+        if(isEven(number)){
+            System.out.print
 }

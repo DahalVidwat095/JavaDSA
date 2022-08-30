f188d06c4fe89b328d3a7e73211b095b0c25ed3e
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-30 23:51:35 +0545

    Incomplete Do While loop

--- src/Functions/InfiniteDoWhileLoop.java
+++ src/Functions/InfiniteDoWhileLoop.java
@@ -1,19 +1,5 @@
 package Functions;
 
-import java.util.Scanner;
-
 public class InfiniteDoWhileLoop{
-/Make a function to check if a given number n is even or not.
-    public static boolean isEven(int number){
-        if(number%2==0){
-            return true;
-        }
-        return false;
-    }
-    public static void main(String args[]){
-        Scanner cs = new Scanner(System.in);
-        System.out.print("Enter your number: ");
-        int number = cs.nextInt();
-        if(isEven(number)){
-            System.out.print
+
 }

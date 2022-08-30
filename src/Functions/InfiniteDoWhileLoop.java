9a1df3a5dcb4eabc4dfdc2cd51eeb057b48e6832
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-30 23:50:50 +0545

    Incomplete Do While loop

--- src/Functions/InfiniteDoWhileLoop.java
+++ src/Functions/InfiniteDoWhileLoop.java
@@ -3,33 +3,14 @@ package Functions;
 import java.util.Scanner;
 
 public class InfiniteDoWhileLoop {
-    public static void printFactorial(int n) {
-        if(n<0){
-            System.out.print("Invalid number.");
-            return;
-        }
-        int factorial = 1;
 
-        for (int i = n; i >= 1; i--) {
-            factorial = factorial * i;
-        }
-        System.out.println("The factorial of " + n + " is " +  factorial +".");
-    }
-    public static void main(String args[]){
-        Scanner cs = new Scanner(System.in);
-        System.out.print("Enter value of n: ");
-        int n = cs.nextInt();
-        printFactorial(n);
-    }
+
+
+
+
+
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
 

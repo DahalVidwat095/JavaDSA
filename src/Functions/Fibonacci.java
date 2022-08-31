cb9d72d2860f673e9fb6877f31ce46bb50b557ce
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-08-31 19:55:51 +0545

    Fibonacci Numbers

--- src/Functions/Fibonacci.java
+++ src/Functions/Fibonacci.java
@@ -3,6 +3,10 @@ package Functions;
 import java.util.Scanner;
 
 public class Fibonacci {
+    //Write a program to print Fibonacci series of n terms where n is input by user :
+    //0 1 1 2 3 5 8 13 21 .....
+    //In the Fibonacci series, a number is the sum of the previous 2 numbers that came before it.
+    //(BONUS)
     public static void fibonacciSequence(int n){
         int number = 0;
         int number2 = 1;

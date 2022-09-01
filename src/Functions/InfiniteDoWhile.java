a62ea76227e9d87f7a5c04b7b6d648a68a140c68
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-09-01 15:18:18 +0545

    Infinite Do While loop

---
+++ src/Functions/InfiniteDoWhile.java
@@ -0,0 +1,15 @@
+package Functions;
+
+
+public class InfiniteDoWhile {
+    public static void infiniteDoWhile(int number){
+        do{
+            System.out.print("*");
+        }while(number<2);{
+        }
+    }
+    public static void main(String args []){
+        int number=1;
+        infiniteDoWhile(number);
+    }
+}

e5d1aa1d87906c3a0ef81df672f6806fd72479de
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-09-15 12:56:27 +0545

    TwoDArrays

---
+++ src/Array/TwoDArray.java
@@ -0,0 +1,24 @@
+package Array;
+import java.util.Scanner;
+public class TwoDArray {
+    public static void main (String args[]){
+        Scanner cs = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = cs.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = cs.nextInt();
+        int[][] numbers = new int[rows][cols];
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                System.out.print("Enter value for "+ i +" row "+ j +" column: " );
+                numbers[i][j] = cs.nextInt();
+            }
+        }
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                System.out.print(numbers[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
